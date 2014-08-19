ee2870549af45158e6e37e5c154a6e5bad6c3854
Jim MacArthur <jim-git@mode7.co.uk>
2014-08-19 23:03:13 +0100

    Add stripped version of LD28 entry

---
+++ loop.js
@@ -0,0 +1,135 @@
+var canvas = document.getElementsByTagName('canvas')[0];
+var ctx = null;
+var body = document.getElementsByTagName('body')[0];
+var keysDown = new Array();
+var SCREENWIDTH  = 640;
+var SCREENHEIGHT = 480;
+var MODE_TITLE = 0;
+var MODE_PLAY  = 1;
+var MODE_WIN   = 2;
+
+function getImage(name)
+{
+    image = new Image();
+    image.src = 'graphics/'+name+'.png';
+    return image;
+}
+
+function drawChar(context, c, x, y) 
+{
+    c = c.charCodeAt(0);
+    if(c > 0) {
+        context.drawImage(bitfont, c*6, 0, 6,8, x, y, 12, 16);
+    }
+}
+
+function drawString(context, string, x, y) {
+    string = string.toUpperCase();
+    for(i = 0; i < string.length; i++) {
+	drawChar(context, string[i], x, y);
+	x += 12;
+    }
+}
+
+function paintTitleBitmaps()
+{
+    drawString(titlectx, 'This is a demo of the JavaScript/HTML5 game loop',32,32);
+    drawString(winctx, 'Your game should always have an ending',32,32);
+}
+
+function makeTitleBitmaps()
+{
+    titleBitmap = document.createElement('canvas');
+    titleBitmap.width = SCREENWIDTH;
+    titleBitmap.height = SCREENHEIGHT;
+    titlectx = titleBitmap.getContext('2d');
+    winBitmap = document.createElement('canvas');
+    winBitmap.width = SCREENWIDTH;
+    winBitmap.height = SCREENHEIGHT;
+    winctx = winBitmap.getContext('2d');
+    bitfont = new Image();
+    bitfont.src = "graphics/bitfont.png";
+    bitfont.onload = paintTitleBitmaps;
+}
+
+function resetGame()
+{
+    x = 128;
+    y = 128;
+}
+
+function init()
+{
+    mode = MODE_TITLE;
+    playerImage = getImage("player");
+    springSound = new Audio("audio/boing.wav");
+    makeTitleBitmaps();
+    return true;
+}
+
+function draw() {
+    ctx.fillStyle = "#0000ff";
+    ctx.fillRect(0, 0, SCREENWIDTH, SCREENHEIGHT);
+
+    if(mode == MODE_TITLE) {
+	ctx.drawImage(titleBitmap, 0, 0);
+	return;
+    }
+
+    ctx.drawImage(playerImage, x, y);
+
+    if(mode == MODE_WIN) {
+	ctx.drawImage(winBitmap, 0, 0);
+    }
+}
+
+function processKeys() {
+    if(keysDown[40] || keysDown[83]) y += 4;
+    if(keysDown[38] || keysDown[87]) y -= 4;
+    if(keysDown[37] || keysDown[65]) x -= 4;
+    if(keysDown[39] || keysDown[68]) x += 4;
+    if(x < 0) x = 0;
+    if(x > SCREENWIDTH - playerImage.width)  x = SCREENHEIGHT - playerImage.width;
+    if(y < 0) y = 0;
+    if(y > SCREENWIDTH - playerImage.height) y = SCREENHEIGHT - playerImage.height;
+}
+
+function drawRepeat() {
+    if(mode != MODE_TITLE) {
+	processKeys();
+    }
+    draw();
+    if(!stopRunloop) setTimeout('drawRepeat()',20);
+}
+
+if (canvas.getContext('2d')) {
+    stopRunloop = false;
+    ctx = canvas.getContext('2d');
+    body.onkeydown = function (event) {
+	var c = event.keyCode;
+        keysDown[c] = 1;
+	if(c == 81) {
+	    stopRunloop=true;
+	}
+	if(c == 32) {
+	    if(mode == MODE_TITLE) {
+		resetGame();
+		mode = MODE_PLAY;
+	    }
+	}
+	if(c == 82) {
+	    if(mode == MODE_WIN) {
+		mode = MODE_TITLE;
+	    }
+	}
+    };
+
+    body.onkeyup = function (event) {
+	var c = event.keyCode;
+        keysDown[c] = 0;
+    };
+
+    if(init()) {      
+      drawRepeat();
+    }
+}
